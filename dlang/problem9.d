6fcd95d4f6d04aaf3ebd7e8cfe2173badb3bc749
Hiroki Noda <kubo39@gmail.com>
2012-06-04 02:47:13 +0900

    solved in d: problem9

---
+++ dlang/problem9.d
@@ -0,0 +1,14 @@
+import std.stdio;
+
+void main(){
+    for (uint a=1;a<333;++a){
+	for (uint b=2;b<500;++b){
+	    if (a>=b) continue;
+	    uint c = 1000 - (a+b);
+	    if (b>=c) continue;
+
+	    if (c*c - (a*a + b*b) == 0)
+		writeln(a*b*c);
+	}
+    }
+}
