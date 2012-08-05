ea39c3d8ac79f28594891959f3e8eeec5e897f3a
Hiroki Noda <kubo39@gmail.com>
2012-08-05 23:17:02 +0900

    solved in d: problem4

---
+++ dlang/problem4.d
@@ -0,0 +1,18 @@
+import std.stdio;
+import std.conv;
+import core.thread;
+
+
+void main(){
+    int str = 0;
+    foreach_reverse(i; 100 .. 999){
+	foreach_reverse(j; 100 .. 999){
+	    string tmp = to!string(i * j);
+	    if (tmp == tmp.dup.reverse) {
+		auto x = to!int(tmp.dup);
+		if(str < x) str = x;
+	    }
+	}
+    }
+    writeln(str);
+}
