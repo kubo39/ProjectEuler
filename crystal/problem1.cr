b51d9162c5e7ed96072236fa589d1f3a818ab701
kubo39 <kubo39@gmail.com>
2015-09-14 05:48:08 +0900

    Simplyfied

--- crystal/problem1.cr
+++ crystal/problem1.cr
@@ -1,21 +1,3 @@
-def power_of_three? n
-  return n % 3 == 0
-end
-
-def power_of_five? n
-  return n % 5 == 0
-end
-
-def main
-  total = 0_u64
-  (1..999).each do |i|
-    if power_of_three?(i)
-      total += i
-    elsif power_of_five?(i)
-      total += i
-    end
-  end
-  puts total
-end
-
-main
+total = 0_u64
+(1..999).each {|i| total += i if i % 3 == 0 || i % 5 == 0 }
+puts total
