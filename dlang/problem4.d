96158ac6626725159b09e56771f1e05194ffb419
kubo39 <kubo39@gmail.com>
2015-09-17 00:03:00 +0900

    Fix indent

--- dlang/problem4.d
+++ dlang/problem4.d
@@ -3,15 +3,15 @@ import std.conv;
 
 
 void main(){
-    int str = 0;
-    foreach_reverse(i; 100 .. 999){
-	foreach_reverse(j; 100 .. 999){
+  int str = 0;
+  foreach_reverse(i; 100 .. 999){
+    foreach_reverse(j; 100 .. 999){
 	    string tmp = to!string(i * j);
 	    if (tmp == tmp.dup.reverse) {
-		auto x = to!int(tmp.dup);
-		if(str < x) str = x;
+        auto x = to!int(tmp.dup);
+        if(str < x) str = x;
 	    }
-	}
     }
-    writeln(str);
+  }
+  writeln(str);
 }
