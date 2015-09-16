15c95fe0112bf92147e5964e488503f9fc9d9fe3
kubo39 <kubo39@gmail.com>
2015-09-17 01:10:02 +0900

    Solved in D: problem8

---
+++ dlang/problem8.d
@@ -0,0 +1,18 @@
+import std.stdio;
+import std.algorithm;
+import std.string;
+import std.conv;
+import std.range;
+
+void main()
+{
+  scope f = File("./problem8.txt");
+  char[] chars = f.byLine.map!chomp.join("");
+
+  ulong x;
+  for (ulong i; (chars.length - i) > 12; ++i) {
+    ulong tmp = chars[i .. i+13].map!((a) => a.to!ulong - 48).reduce!((a, b) => a * b);
+    if (tmp > x) x = tmp;
+  }
+  x.writeln;
+}
