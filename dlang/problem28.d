80adf7e36d16c35b080a26ee528f06e34ae53c43
kubo39 <kubo39@gmail.com>
2015-09-16 23:07:10 +0900

    Solved in D: problem 28

---
+++ dlang/problem28.d
@@ -0,0 +1,22 @@
+import std.stdio;
+
+ulong f(int n)
+{
+  ulong total = 1;
+  for (int i=3; i <= n; i+=2) {
+    total += 4 * i * i - 6*(i - 1);
+  }
+  return total;
+}
+
+unittest
+{
+  assert(f(3) == 25);
+  assert(f(5) == 101);
+}
+
+
+void main()
+{
+  f(1001).writeln;
+}
