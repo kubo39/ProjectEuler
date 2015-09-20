b67da4ebe441946b43b9378bcbb2154bc94deebc
kubo39 <kubo39@gmail.com>
2015-09-21 05:40:06 +0900

    Indent

--- dlang/problem1.d
+++ dlang/problem1.d
@@ -1,9 +1,10 @@
 import std.stdio;
 
-void main(){
-    int count;
-    uint i;
-    for(i=1;i<1000;++i)
-	if(i % 3 == 0 || i % 5 == 0) count += i;
-    writeln(count);
+void main()
+{
+  int count;
+  uint i;
+  for(i=1;i<1000;++i)
+    if(i % 3 == 0 || i % 5 == 0) count += i;
+  count.writeln;
 }
