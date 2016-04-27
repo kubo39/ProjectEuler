164ef978a5888a0f68bfff825bd6099632a4d75d
kubo39 <kubo39@gmail.com>
2016-04-28 01:58:09 +0900

    dlang: rewrite problem2.

--- dlang/problem2.d
+++ dlang/problem2.d
@@ -2,9 +2,11 @@ import std.stdio;
 
 void main()
 {
-  ulong total;
-  for (int x=1,y=1,z=0;x<4000000;z=x,x+=y,y=z) {
-    if(x % 2 == 0) total += x;
-  }
-  total.writeln;
+    ulong total;
+    for (int x=1,y=1,z=0; x<4_000_000; z=x, x+=y, y=z)
+    {
+        if(x % 2 == 0)
+            total += x;
+    }
+    total.writeln;
 }
