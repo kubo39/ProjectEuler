3c0102c3ce3f2c455340151bc03ce72fef363f9d
kubo39 <kubo39@gmail.com>
2016-04-28 02:20:12 +0900

    dlang: rewrite problem28.

--- dlang/problem28.d
+++ dlang/problem28.d
@@ -2,21 +2,22 @@ import std.stdio;
 
 ulong f(int n)
 {
-  ulong total = 1;
-  for (int i=3; i <= n; i+=2) {
-    total += 4 * i * i - 6*(i - 1);
-  }
-  return total;
+    ulong total = 1;
+    for (int i=3; i <= n; i+=2)
+    {
+        total += 4 * i * i - 6*(i - 1);
+    }
+    return total;
 }
 
 unittest
 {
-  assert(f(3) == 25);
-  assert(f(5) == 101);
+    assert(f(3) == 25);
+    assert(f(5) == 101);
 }
 
 
 void main()
 {
-  f(1001).writeln;
+    f(1001).writeln;
 }
